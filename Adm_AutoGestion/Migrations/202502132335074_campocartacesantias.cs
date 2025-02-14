006fd40db4981e5be69b5818ee5b838d40b0c2bb
andres rincon <andres.rincon2@gmail.com>
2025-02-13 21:05:23 -0500

    cambios

---
+++ Adm_AutoGestion/Migrations/202502132335074_campocartacesantias.cs
@@ -0,0 +1,18 @@
+﻿namespace Adm_AutoGestion.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class campocartacesantias : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.SolicitudCesantias", "CartaFondo", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.SolicitudCesantias", "CartaFondo");
+        }
+    }
+}
